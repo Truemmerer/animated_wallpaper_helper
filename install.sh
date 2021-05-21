4cb07aa1b2fd08b664a8e57e64a7ccc223b8d23f
Truemmerer <truemmerer@gmail.com>
2021-05-21 22:16:14 +0200

    initial fedora installscript release

--- install.sh
+++ install.sh
@@ -1,25 +1,39 @@
 #!/bin/bash
 
-if (whoami != root)
-  then echo "Please run as root"
+if [ $(whoami) != 'root' ]; then
+  echo "Please run as root"
 
   else
 
-# Detect OS
-if [[ -e /etc/fedora-release ]]; then
-	os="fedora"
-	os_version=$(grep -oE '[0-9]+' /etc/fedora-release | head -1)
+        # Detect OS
+if [ -f /etc/os-release ]; then
+        # freedesktop.org and systemd
+        . /etc/os-release
+    OS=$NAME
+elif type lsb_release >/dev/null 2>&1; then
+    OS=$(lsb_release -si)
+elif [ -f /etc/lsb-release ]; then
+    . /etc/lsb-release
+    OS=$DISTRIB_ID
+else
+    # Fall back to uname, e.g. "Linux <version>", also works for BSD, etc.
+    OS=$(uname -s)
+    VER=$(uname -r)
 fi
 
 # Install Dependencies
-		if [[ "$os" == "fedora" ]]; then
-			# Fedora
-			echo Install Fedora Dependencies 
-			dnf install -y cmake vala pkgconfig gkt3-devel clutter-devel clutter-gtk clutter-gst3-devel youtube-dl 
-		fi
+                if [ "$OS" == "Fedora" ]; then
+                        # Fedora
+                        echo Install Fedora Dependencies
+                        echo Add rpmfusion repository for ffmpeg
+                        dnf install -y https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm 
+                        echo Install dev tools and dependencies
+                        dnf install -y cmake gcc-c++ vala pkgconfig gtk3-devel clutter-devel clutter-gtk-devel clutter-gst3-devel youtube-dl
+                fi
+
 
 # Clone and Install animated-wallpaper
-echo Clone animated-wallpaper from github. (https://github.com/Ninlives/animated-wallpaper)
+echo 'Clone animated-wallpaper from github. (https://github.com/Ninlives/animated-wallpaper)'
 
 git clone https://github.com/Ninlives/animated-wallpaper
 cd animated-wallpaper
@@ -29,11 +43,10 @@ rm -rf animated-wallpaper
 
 # Clone and Install animated_wallpaper_helper
 
-mv awp /usr/local/share/
-mv animation-wallpaper.desktop /usr/share/applications/
+cp -r awp /usr/local/share/
+cp animated-wallpaper.desktop /usr/share/applications/
 chmod +x /usr/local/share/awp/awp.sh
 chmod +x /usr/local/share/awp/awp_autostart.sh
 
 fi
 
-exit
